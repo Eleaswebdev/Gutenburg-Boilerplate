69109b0f56545a48c269e9eb1821a6a54c1aa739
Eleas Kanchon <eleas@softzino.com>
2025-05-07 11:29:32 +0600

    Update plugin header

--- plugin.php
+++ plugin.php
@@ -5,8 +5,8 @@
  * Requires at least: 6.0
  * Requires PHP:      7.4
  * Version:           1.0.0
- * Author:            Zakaria Binsaifullah
- * Author URI:        https://makegutenblock.com
+ * Author:            Eleas Kanchon
+ * Author URI:        https://github.com/Eleaswebdev
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       boilerplate
